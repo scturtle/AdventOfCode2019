fd38a02739e5e81c55e5dedbda6766bc49e78f7c
shenchao <shenchao@megvii.com>
2019-12-23 13:55:50 +0800

    day23

--- src/day23.rs
+++ src/day23.rs
@@ -1,7 +1,6 @@
-#![allow(dead_code)]
 use std::collections::VecDeque;
 
-pub struct Intcode {
+struct Intcode {
     mem: Vec<i64>,
     i: usize, // pc
     base: i32,
@@ -9,7 +8,7 @@ pub struct Intcode {
 }
 
 impl Intcode {
-    pub fn new(memsize: usize) -> Self {
+    fn new(memsize: usize) -> Self {
         Self {
             mem: vec![0; memsize],
             i: 0,
@@ -18,17 +17,21 @@ impl Intcode {
         }
     }
 
-    pub fn reset(&mut self, codes: &[i64]) {
+    fn reset(&mut self, codes: &[i64]) {
         self.i = 0;
         self.base = 0;
         self.mem[..codes.len()].clone_from_slice(&codes[..]);
     }
 
-    pub fn push<I>(&mut self, inputs: I)
+    fn push<I>(&mut self, inputs: I)
     where
         I: IntoIterator<Item = i64>,
     {
         self.input.extend(inputs);
+    }
+
+    fn is_empty(&self) -> bool {
+        self.input.is_empty()
     }
 
     fn get_addr(&self, n: u32) -> usize {
@@ -61,8 +64,12 @@ impl Intcode {
                 self.i += 4;
             } else if inst == 3 {
                 let out = out1();
-                self.mem[out] = self.input.pop_front().unwrap();
+                let input = self.input.pop_front().unwrap_or(-1);
+                self.mem[out] = input;
                 self.i += 2;
+                if input == -1 {
+                    return None;
+                }
             } else if inst == 4 {
                 let output = val1();
                 self.i += 2;
@@ -100,8 +107,63 @@ impl Intcode {
                 self.i += 2;
             } else {
                 assert_eq!(inst, 99);
-                return None;
+                unreachable!();
             }
+        }
+    }
+}
+
+pub fn run() {
+    let codes = crate::common::get_input(23)
+        .unwrap()
+        .trim_end()
+        .split(',')
+        .map(|i| i.parse::<i64>().unwrap())
+        .collect::<Vec<_>>();
+
+    let mut computers: Vec<Intcode> = (0..50)
+        .map(|addr| {
+            let mut intcode = Intcode::new(10000);
+            intcode.reset(&codes);
+            intcode.push(vec![addr as i64]);
+            intcode
+        })
+        .collect();
+
+    let mut last_nat_sent_y = 0;
+    let mut net_pkt = (0, 0);
+    let mut last_no_outs = false;
+    loop {
+        let mut outs = vec![];
+        for c in &mut computers {
+            if let Some(o1) = c.step() {
+                let o2 = c.step().unwrap();
+                let o3 = c.step().unwrap();
+                outs.push((o1, o2, o3));
+            }
+        }
+        for &(addr, x, y) in &outs {
+            if addr == 255 {
+                if net_pkt.1 == 0 {
+                    dbg!(y);
+                }
+                net_pkt = (x, y);
+            } else {
+                computers[addr as usize].push(vec![x, y]);
+            }
+        }
+        if outs.is_empty() {
+            if last_no_outs && computers.iter().all(|c| c.is_empty()) {
+                computers[0].push(vec![net_pkt.0, net_pkt.1]);
+                if last_nat_sent_y == net_pkt.1 {
+                    dbg!(last_nat_sent_y);
+                    break;
+                }
+                last_nat_sent_y = net_pkt.1;
+            }
+            last_no_outs = true;
+        } else {
+            last_no_outs = false;
         }
     }
 }
